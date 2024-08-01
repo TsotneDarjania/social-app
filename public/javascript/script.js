e1d0e8127f0e8d85897dacbf0284261f7feefd1d
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2024-08-01 12:35:33 +0400

    login form added

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,1 +1,9 @@
-console.log("working...");
+const showSignUpForm = () => {
+  document.querySelector(".login-form").style.display = "none";
+  document.querySelector(".sign-up-form").style.display = "block";
+};
+
+const showLoginForm = () => {
+  document.querySelector(".sign-up-form").style.display = "none";
+  document.querySelector(".login-form").style.display = "block";
+};
--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,11 @@
-import http from "http";
-import fs from "fs";
+import express from "express";
 import { PORT } from "./config";
 
-console.log("working");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  const filePath = () => {
-    if (url === "/") {
-      return "public/index.html";
-    } else if (url === "/javascript/script.js") {
-      console.log("script.js");
-      return "public/javascript/script.js";
-    } else if (url === "/style/style.css") {
-      return "public/style/style.css";
-    } else if (url === "/favicon.ico") {
-      return "public/assets/logo.png";
-    } else {
-      return "";
-    }
-  };
-
-  fs.readFile(filePath(), (err, data) => {
-    if (err) {
-      res.end("Error");
-    } else {
-      res.end(data);
-    }
-  });
-});
+const app = express();
+app.use(express.static("public"));
 
 export const startServer = () => {
-  server.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
 };
