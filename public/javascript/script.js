2829161804118cf6008ae9a0ea3e00d053bc8e99
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2024-08-01 13:05:59 +0400

    sign-up form added

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,9 +1,9 @@
 const showSignUpForm = () => {
-  document.querySelector(".login-form").style.display = "none";
-  document.querySelector(".sign-up-form").style.display = "block";
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("sign-up-form").style.display = "block";
 };
 
 const showLoginForm = () => {
-  document.querySelector(".sign-up-form").style.display = "none";
-  document.querySelector(".login-form").style.display = "block";
+  document.getElementById("sign-up-form").style.display = "none";
+  document.getElementById("login-form").style.display = "block";
 };
