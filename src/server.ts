ab6b7aee849d0b2263dfc17f46abda857c788935
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2024-08-01 13:21:53 +0400

    submit functionality added

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,25 @@ import { PORT } from "./config";
 const app = express();
 app.use(express.static("public"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/login", (req, res) => {
+  const data = req.body;
+
+  // console.log("data", data);
+
+  res.json(data);
+});
+
+app.post("/sign-up", (req, res) => {
+  const data = req.body;
+
+  // console.log("data", data);
+
+  res.json(data);
+});
+
 export const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
